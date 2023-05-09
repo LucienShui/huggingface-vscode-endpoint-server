903a4294bcc412de754299a16275e62452a68a64
Lucien <lucien@lucien.ink>
2023-05-10 01:56:35 +0800

    revert to pipeline, fix model output not work

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from transformers import GenerationConfig
+from transformers import GenerationConfig, Pipeline, pipeline
+import torch
+import json
 
-from util import logger, from_pretrained
+from util import logger, get_parser
 
 app = FastAPI()
-
 app.add_middleware(
     CORSMiddleware
 )
 
-pretrained = "bigcode/starcoder"
-generation_config: GenerationConfig = GenerationConfig.from_pretrained(pretrained)
-model, tokenizer = from_pretrained(pretrained)
+generation_config: GenerationConfig = ...
+pipe: Pipeline = ...
 
 
 def generate(inputs: str, parameters: dict) -> str:
     config = GenerationConfig.from_dict({
         **generation_config.to_dict(),
-        **{"pad_token_id": tokenizer.eos_token_id},
+        **{"pad_token_id": pipe.tokenizer.eos_token_id},
         **parameters}
     )
-
-    encoded_ids = tokenizer.encode(inputs, return_tensors="pt").to('cuda:0')
-    output_ids = model.generate(encoded_ids, generation_config=config)
-    response = tokenizer.decode(output_ids[0])
-    return response
+    json_response: dict = pipe(inputs, generation_config=config)[0]
+    generated_text: str = json_response['generated_text']
+    return generated_text
 
 
 @app.post("/api/generate/")
@@ -34,9 +32,9 @@ async def api(request: Request):
     json_request: dict = await request.json()
     inputs: str = json_request['inputs']
     parameters: dict = json_request['parameters']
-    logger.info(f'{request.client.host}:{request.client.port} inputs = {inputs}')
+    logger.info(f'{request.client.host}:{request.client.port} inputs = {json.dumps(inputs)}')
     generated_text: str = generate(inputs, parameters)
-    logger.info(f'{request.client.host}:{request.client.port} generated_text = {generated_text}')
+    logger.info(f'{request.client.host}:{request.client.port} generated_text = {json.dumps(generated_text)}')
     return {
         "generated_text": generated_text,
         "status": 200
@@ -44,7 +42,11 @@ async def api(request: Request):
 
 
 def main():
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    global generation_config, pipe
+    args = get_parser().parse_args()
+    generation_config = GenerationConfig.from_pretrained(args.pretrained)
+    pipe = pipeline("text-generation", model=args.pretrained, torch_dtype=torch.float16, device_map='auto')
+    uvicorn.run(app, host=args.host, port=args.port)
 
 
 if __name__ == '__main__':
--- util.py
+++ util.py
@@ -1,9 +1,5 @@
 import logging
-from typing import Tuple
-
-import torch
-from accelerate import infer_auto_device_map, init_empty_weights, load_checkpoint_and_dispatch
-from transformers import GPT2TokenizerFast, GPTBigCodeForCausalLM, GPTBigCodeConfig
+import argparse
 
 logging.basicConfig(
     format="%(asctime)s %(levelname)s %(message)s",
@@ -17,19 +13,9 @@ logging.basicConfig(
 logger = logging.getLogger('app')
 
 
-def from_pretrained(pretrained: str, distributed: bool = False, max_memory: dict = None,
-                    half: bool = True) -> Tuple[GPTBigCodeForCausalLM, GPT2TokenizerFast]:
-    if distributed:
-        config = GPTBigCodeConfig.from_pretrained(pretrained)
-        with init_empty_weights():
-            model: GPTBigCodeForCausalLM = GPTBigCodeForCausalLM.from_config(config)
-        device_map = infer_auto_device_map(model, max_memory=max_memory)
-        logger.info(f'device_map = {device_map}')
-        model.tie_weights()
-        model: GPTBigCodeForCausalLM = load_checkpoint_and_dispatch(
-            model, pretrained, device_map=device_map, dtype=torch.float16)
-    else:
-        model: GPTBigCodeForCausalLM = GPTBigCodeForCausalLM.from_pretrained(pretrained)
-        model: GPTBigCodeForCausalLM = model.half().cuda() if half else model.cuda()
-    tokenizer: GPT2TokenizerFast = GPT2TokenizerFast.from_pretrained(pretrained)
-    return model, tokenizer
+def get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', type=int, default=8000)
+    parser.add_argument('--host', type=str, default='0.0.0.0')
+    parser.add_argument('--pretrained', type=str, required=True)
+    return parser
