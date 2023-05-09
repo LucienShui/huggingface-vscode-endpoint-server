9a9be8bc911b45028914874c4949b4555e55553c
Lucien <lucien@lucien.ink>
2023-05-09 23:06:01 +0800

    callable

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig, pipeline, Pipeline
+from transformers import GenerationConfig, pipeline, Pipeline
 import torch
 
 logging.basicConfig(
@@ -15,17 +15,22 @@ logging.basicConfig(
     ]
 )
 
-pretrained = "bigcode/starcoder"
-tokenizer = AutoTokenizer.from_pretrained(pretrained, trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(pretrained, trust_remote_code=True)
-pipe: Pipeline = pipeline("text-generation", model=model, tokenizer=tokenizer, device_map="auto")
+logger = logging.getLogger(__name__)
+
+try:
+    import accelerate
+except (ImportError, ModuleNotFoundError):
+    logger.warning('accelerate not installed, would not dispatch model to GPUs automatically.')
 
 app = FastAPI()
-logger = logging.getLogger(__name__)
 
 app.add_middleware(
     CORSMiddleware
 )
+
+pretrained = "bigcode/starcoder"
+generation_config: GenerationConfig = GenerationConfig.from_pretrained(pretrained)
+pipe: Pipeline = pipeline("text-generation", model=pretrained, torch_dtype=torch.bfloat16, device_map='auto')
 
 
 @app.post("/api/generate/")
@@ -33,9 +38,14 @@ async def chat(request: Request):
     json_request: dict = await request.json()
     inputs: str = json_request['inputs']
     parameters: dict = json_request['parameters']
-    config: GenerationConfig = GenerationConfig(**parameters)
+    config = GenerationConfig.from_dict({
+        **generation_config.to_dict(),
+        **{"pad_token_id": pipe.tokenizer.eos_token_id},
+        **parameters}
+    )
     logger.info(f'{request.client.host}:{request.client.port} inputs = {inputs}')
     generated_text: str = pipe(inputs, generation_config=config)
+    logger.info(f'{request.client.host}:{request.client.port} generated_text = {generated_text}')
     return {
         "generated_text": generated_text,
         "status": 200
